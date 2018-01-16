424852951aa0c306d466f249f111a4f06d3dfc26
hoelzlem <markus.hoelzle@gmail.com>
2018-01-16 15:25:59 +0100

    Even more changes to parser

--- src/config_parser.ads
+++ src/config_parser.ads
@@ -1,10 +1,10 @@
-pragma Profile(Ravenscar);
+pragma Profile (Ravenscar);
 with Ada.Text_IO; use Ada.Text_IO;
 with PSU_Simulation; use PSU_Simulation;
 with PSU_Control; use PSU_Control;
 
 package CONFIG_Parser is
 
-   procedure parseConfig (fileHandle : in File_Type; PID_Config : in out PID_Config_A_T; Sim_Config : in out Sim_Config_T; configValid : out Boolean);
-   procedure parseLoad (fileHandle : in File_Type; loadArray : out loadArray_T; loadValid : out Boolean);
+    procedure parseConfig (fileHandle : in File_Type; PID_Config : in out PID_Config_A_T; Sim_Config : in out Sim_Config_T; configValid : out Boolean);
+    procedure parseLoad (fileHandle : in File_Type; loadArray : out loadArray_T; loadValid : out Boolean);
 end CONFIG_Parser;
