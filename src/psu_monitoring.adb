62338bb34b6120ccd77182b2210c2f9f000be3a1
Simon Diehl <j.simon.diehl@googlemail.com>
2018-01-16 16:06:53 +0100

    * Separated monitor config and state variables ** Reverted a lot of previous changes to the protected type ** Added monitoring type to hold config and state variables ** Added global variables for all four monitored signals
    Compiles and runs

--- src/psu_monitoring.adb
+++ src/psu_monitoring.adb
@@ -1,77 +1,84 @@
 pragma Profile(Ravenscar);
 pragma SPARK_Mode;
 
+with Ada.Text_IO; use Ada.Text_IO;
+
 with PSU_Simulation; use PSU_Simulation;
 
 package body PSU_Monitoring is
 
 protected body Monitoring_Interface_T is
 
-    function is_all_monitors_set return Boolean is
+    function is_all_config_set return Boolean is
     begin
-        return monitor_pfc_voltage_set and
-               monitor_pfc_current_set and
-               monitor_output_voltage_set and
-               monitor_output_current_set;
-    end is_all_monitors_set;
+        return monitor_pfc_voltage_config_set and
+               monitor_pfc_current_config_set and
+               monitor_output_voltage_config_set and
+               monitor_output_current_config_set;
+    end is_all_config_set;
 
-    procedure set_monitor_pfc_voltage(new_monitor : in Monitor_T) is
+    procedure set_monitor_pfc_voltage_config(new_monitor_config : in Monitor_Config_T) is
     begin
-        monitor_pfc_voltage := new_monitor;
-        monitor_pfc_voltage_set := True;
-    end set_monitor_pfc_voltage;
+        monitor_pfc_voltage_config := new_monitor_config;
+        monitor_pfc_voltage_config_set := True;
+    end set_monitor_pfc_voltage_config;
 
-    function get_monitor_pfc_voltage return Monitor_T is
+    function get_monitor_pfc_voltage_config return Monitor_Config_T is
     begin
-        return monitor_pfc_voltage;
-    end get_monitor_pfc_voltage;
+        return monitor_pfc_voltage_config;
+    end get_monitor_pfc_voltage_config;
 
-    procedure set_monitor_pfc_current(new_monitor : in Monitor_T) is
+    procedure set_monitor_pfc_current_config(new_monitor_config : in Monitor_Config_T) is
     begin
-        monitor_pfc_current := new_monitor;
-        monitor_pfc_current_set := True;
-    end set_monitor_pfc_current;
+        monitor_pfc_current_config := new_monitor_config;
+        monitor_pfc_current_config_set := True;
+    end set_monitor_pfc_current_config;
 
-    function get_monitor_pfc_current return Monitor_T is
+    function get_monitor_pfc_current_config return Monitor_Config_T is
     begin
-        return monitor_pfc_current;
-    end get_monitor_pfc_current;
+        return monitor_pfc_current_config;
+    end get_monitor_pfc_current_config;
 
-    procedure set_monitor_output_voltage(new_monitor : in Monitor_T) is
+    procedure set_monitor_output_voltage_config(new_monitor_config : in Monitor_Config_T) is
     begin
-        monitor_output_voltage := new_monitor;
-        monitor_output_voltage_set := True;
-    end set_monitor_output_voltage;
+        monitor_output_voltage_config := new_monitor_config;
+        monitor_output_voltage_config_set := True;
+    end set_monitor_output_voltage_config;
 
-    function get_monitor_output_voltage return Monitor_T is
+    function get_monitor_output_voltage_config return Monitor_Config_T is
     begin
-        return monitor_output_voltage;
-    end get_monitor_output_voltage;
+        return monitor_output_voltage_config;
+    end get_monitor_output_voltage_config;
 
-    procedure set_monitor_output_current(new_monitor : in Monitor_T) is
+    procedure set_monitor_output_current_config(new_monitor_config : in Monitor_Config_T) is
     begin
-        monitor_output_current := new_monitor;
-        monitor_output_current_set := True;
-    end set_monitor_output_current;
+        monitor_output_current_config := new_monitor_config;
+        monitor_output_current_config_set := True;
+    end set_monitor_output_current_config;
 
-    function get_monitor_output_current return Monitor_T is
+    function get_monitor_output_current_config return Monitor_Config_T is
     begin
-        return monitor_output_current;
-    end get_monitor_output_current;
+        return monitor_output_current_config;
+    end get_monitor_output_current_config;
 
 end Monitoring_Interface_T;
 
 task body Monitoring_Task_T is
-    TASK_PERIOD : constant Time_Span := Milliseconds(1);
     next_time : Time;
+
 begin
     -- Initialisation of next execution time
     next_time := Clock;
     -- Superloop
     loop
+        -- Load monitor configuration
+        monitor_pfc_voltage.config := monitoring_interface.get_monitor_pfc_voltage_config;
+        monitor_pfc_current.config := monitoring_interface.get_monitor_pfc_current_config;
+        monitor_output_voltage.config := monitoring_interface.get_monitor_output_voltage_config;
+        monitor_output_current.config := monitoring_interface.get_monitor_output_current_config;
         -- Check if module has been configured correctly
         -- Don't do anything otherwise
-        if monitoring_interface.is_all_monitors_set = True then
+        if monitoring_interface.is_all_config_set = True then
             do_monitoring;
         end if;
 
@@ -80,41 +87,58 @@ begin
     end loop;
 end Monitoring_Task_T;
 
--- Create task => system runs it
-monitoring_task : Monitoring_Task_T;
-
 procedure do_monitoring is
 begin    
     -- Monitor PFC intermediate voltage
-    monitor_signal(monitoring_interface.get_monitor_pfc_voltage, Sim.Get_U_C1);
+    monitor_signal(monitor_pfc_voltage, Sim.Get_U_C1);
     -- Monitor PFC inductor current
-    monitor_signal(monitoring_interface.get_monitor_pfc_current, Sim.Get_I_L1);
+    monitor_signal(monitor_pfc_current, Sim.Get_I_L1);
     -- Monitor output voltage
-    monitor_signal(monitoring_interface.get_monitor_output_voltage, Sim.Get_U_C2);
+    monitor_signal(monitor_output_voltage, Sim.Get_U_C2);
     -- Monitor output inductor current
-    monitor_signal(monitoring_interface.get_monitor_output_current, Sim.Get_I_L2);    
+    monitor_signal(monitor_output_current, Sim.Get_I_L2);    
 end do_monitoring;
 
-procedure monitor_signal(monitor : in Monitor_T; signal_value : in Float) is
+procedure monitor_signal(monitor : in out Monitor_T; signal_value : in Float) is
 begin
     -- @TODO add some code that leaves some error margin for controller startup! Otherwise the controller would be overriden as soon as the monitoring tasks starts since the signals will always violate the limits during startup. Maybe some kind of timer that is reset when the signals stabilise within the limits. After shutting down the controller the monitoring system should allow at least one retry.
-    -- First check whether signal is within limits
-    if is_within_limits(monitor, signal_value) = False then
-        -- @TODO call a function from controller module that disables the power stage
-        null;
-    end if;
+
+    -- Update monitor state
+    monitor.current_state := monitor.next_state;
+
+    case monitor.current_state is
+        when startup =>
+            -- @TODO
+            null;
+        when settling =>
+            -- @TODO
+            null;
+        when active =>
+            if is_within_limits(monitor, signal_value) = False then
+                -- @TODO call a function from controller module that disables the power stage
+                monitor.next_state := alert;
+            else
+                monitor.next_state := active;
+            end if;
+        when alert =>
+            -- @TODO
+            null;
+        when shutdown =>
+            -- @TODO
+            null;
+    end case;
 end monitor_signal;
 
 function is_within_limits(monitor : in Monitor_T; signal_value : in Float) return Boolean is
     within_limits : Boolean := False;
 begin
-    case monitor.monitoring_mode is
+    case monitor.config.monitoring_mode is
         when mean_based =>
-            if abs(monitor.mean - signal_value) < monitor.maximum_deviation then
+            if abs(monitor.config.mean - signal_value) < monitor.config.maximum_deviation then
                 within_limits := True;
             end if;
         when threshold_based =>
-            if signal_value >= monitor.lower_threshold or signal_value <= monitor.upper_threshold then
+            if signal_value >= monitor.config.lower_threshold or signal_value <= monitor.config.upper_threshold then
                 within_limits := True;
             end if;
     end case;
--- src/psu_monitoring.ads
+++ src/psu_monitoring.ads
@@ -6,12 +6,12 @@ with Ada.Real_Time; use Ada.Real_Time;
 package PSU_Monitoring is
 
     type Monitoring_Mode_T is (mean_based, threshold_based);
-    type Monitor_State_T is (startup, settling, active, shutdown);
+    type Monitor_State_T is (startup, settling, active, alert, shutdown);
     
     subtype Float_Natural1000 is Float range 0.0..1000.0;
     subtype Float_Signed1000 is Float range -1000.0..1000.0;
 
-    type Monitor_T is record
+    type Monitor_Config_T is record
         monitoring_mode : Monitoring_Mode_T := mean_based;
         mean : Float_Signed1000 := 0.0;
         maximum_deviation : Float_Natural1000 := 0.0;
@@ -21,37 +21,42 @@ package PSU_Monitoring is
         settling_time : Time_Span := Milliseconds(2);
         violation_time : Time_Span := Milliseconds(5);
         retry_time : Time_Span := Milliseconds(100);
+    end record;
+
+    type Monitor_T is record
+        config : Monitor_Config_T;
         timer : Time_Span := Milliseconds(0);
-        state : Monitor_State_T := startup;
+        current_state : Monitor_State_T := startup;
+        next_state : Monitor_State_T := startup;
     end record;
 
     protected type Monitoring_Interface_T is
-        function is_all_monitors_set return Boolean;
+        function is_all_config_set return Boolean;
 
-        procedure set_monitor_pfc_voltage(new_monitor : in Monitor_T);
-        function get_monitor_pfc_voltage return Monitor_T;
+        procedure set_monitor_pfc_voltage_config(new_monitor_config : in Monitor_Config_T);
+        function get_monitor_pfc_voltage_config return Monitor_Config_T;
         
-        procedure set_monitor_pfc_current(new_monitor : in Monitor_T);
-        function get_monitor_pfc_current return Monitor_T;
+        procedure set_monitor_pfc_current_config(new_monitor_config : in Monitor_Config_T);
+        function get_monitor_pfc_current_config return Monitor_Config_T;
         
-        procedure set_monitor_output_voltage(new_monitor : in Monitor_T);
-        function get_monitor_output_voltage return Monitor_T;
+        procedure set_monitor_output_voltage_config(new_monitor_config : in Monitor_Config_T);
+        function get_monitor_output_voltage_config return Monitor_Config_T;
         
-        procedure set_monitor_output_current(new_monitor : in Monitor_T);
-        function get_monitor_output_current return Monitor_T;
+        procedure set_monitor_output_current_config(new_monitor_config : in Monitor_Config_T);
+        function get_monitor_output_current_config return Monitor_Config_T;
     private
         -- Configuration for PFC intermediate voltage
-        monitor_pfc_voltage : Monitor_T;
-        monitor_pfc_voltage_set : Boolean := False;
+        monitor_pfc_voltage_config : Monitor_Config_T;
+        monitor_pfc_voltage_config_set : Boolean := False;
         -- Configuration for PFC inductor current
-        monitor_pfc_current : Monitor_T;
-        monitor_pfc_current_set : Boolean := False;
+        monitor_pfc_current_config : Monitor_Config_T;
+        monitor_pfc_current_config_set : Boolean := False;
         -- Configuration for output voltage
-        monitor_output_voltage : Monitor_T;
-        monitor_output_voltage_set : Boolean := False;
+        monitor_output_voltage_config : Monitor_Config_T;
+        monitor_output_voltage_config_set : Boolean := False;
         -- Configuration for output inductor current
-        monitor_output_current : Monitor_T;
-        monitor_output_current_set : Boolean := False;
+        monitor_output_current_config : Monitor_Config_T;
+        monitor_output_current_config_set : Boolean := False;
     end Monitoring_Interface_T;
 
     monitoring_interface : Monitoring_Interface_T;
@@ -59,8 +64,17 @@ package PSU_Monitoring is
 private
     task type Monitoring_Task_T;
 
+    monitoring_task : Monitoring_Task_T;
+
+    TASK_PERIOD : constant Time_Span := Milliseconds(100);
+
+    monitor_pfc_voltage : Monitor_T;
+    monitor_pfc_current : Monitor_T;
+    monitor_output_voltage : Monitor_T;
+    monitor_output_current : Monitor_T;
+
     procedure do_monitoring;
-    procedure monitor_signal(monitor : in Monitor_T; signal_value : in Float);
+    procedure monitor_signal(monitor : in out Monitor_T; signal_value : in Float);
     function is_within_limits(monitor : in Monitor_T; signal_value : in Float) return Boolean;
 
 end PSU_Monitoring;
