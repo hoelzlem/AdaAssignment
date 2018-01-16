72cc5975d931161bc984f8e7d27b8c463440dce8
hoelzlem <markus.hoelzle@gmail.com>
2018-01-16 14:54:34 +0100

    Added interface for monitoring Some fixes for Stylechecks

--- src/psu_simulation.adb
+++ src/psu_simulation.adb
@@ -1,131 +1,139 @@
 pragma Profile (Ravenscar);
--- with System.Dim.Mks - Would be nice to use here
+--  with System.Dim.Mks - Would be nice to use here
 with Ada.Real_Time; use Ada.Real_Time;
 with Ada.Numerics.Elementary_Functions;
 with Ada.Numerics;
 with Ada.Text_IO;
 package body PSU_Simulation is
 
-  protected body Simulation_I_T is
+   protected body Simulation_I_T is
 
-    entry Wait_For_Config  when Conf_OK is
-    begin
-      null;
-    end Wait_For_Config;
+      function Is_Ready return Boolean is
+      begin
+         return Conf_OK;
+      end Is_Ready;
 
-    function Is_Ready return Boolean is
-    begin
-      return Conf_OK;
-    end Is_Ready;
+      function Get_Config return Sim_Config_T is
+      begin
+         return Conf;
+      end Get_Config;
 
-    function Get_Config return Sim_Config_T is
-    begin
-      return Conf;
-    end Get_Config;
+      function Get_U_V1 return Float is
+      begin
+         return Sim_Out.U_V1;
+      end Get_U_V1;
 
-    function Get_U_V1 return Float is
-    begin
-      return Sim_Out.U_V1;
-    end Get_U_V1;
+      function Get_I_L1 return Float is
+      begin
+         return Sim_Out.I_L1;
+      end Get_I_L1;
 
-    function Get_I_L1 return Float is
-    begin
-      return Sim_Out.I_L1;
-    end Get_I_L1;
+      function Get_U_C1 return Float is
+      begin
+         return Sim_Out.U_C1;
+      end Get_U_C1;
 
-    function Get_U_C1 return Float is
-    begin
-      return Sim_Out.U_C1;
-    end Get_U_C1;
+      function Get_I_L2 return Float is
+      begin
+         return Sim_Out.I_L2;
+      end Get_I_L2;
 
-    function Get_I_L2 return Float is
-    begin
-      return Sim_Out.I_L2;
-    end Get_I_L2;
+      function Get_U_C2 return Float is
+      begin
+         return Sim_Out.U_C2;
+      end Get_U_C2;
 
-    function Get_U_C2 return Float is
-    begin
-      return Sim_Out.U_C2;
-    end Get_U_C2;
+      function Get_I_Load return Float is
+      begin
+         return Sim_Out.I_Load;
+      end Get_I_Load;
 
-    function Get_I_Load return Float is
-    begin
-      return Sim_Out.I_Load;
-    end Get_I_Load;
+      function Get_Sim_All return Sim_Output_T is
+      begin
+         return Sim_Out;
+      end Get_Sim_All;
 
-    function Get_Sim_All return Sim_Output_T is
-    begin
-      return Sim_Out;
-    end Get_Sim_All;
+      function Get_D_M1 return Float is
+      begin
+         return D_M1;
+      end Get_D_M1;
 
-    function Get_D_M1 return Float is
-    begin
-      return D_M1;
-    end Get_D_M1;
+      function Get_D_M2_5 return Float is
+      begin
+         return D_M2_5;
+      end Get_D_M2_5;
 
-    function Get_D_M2_5 return Float is
-    begin
-      return D_M2_5;
-    end Get_D_M2_5;
+      procedure Set_Config (Val : in Sim_Config_T) is
+      begin
+         Conf    := Val;
+         Conf_OK := True;
+      end Set_Config;
 
-    procedure Set_Config (Val : in Sim_Config_T) is
-    begin
-      Conf    := Val;
-      Conf_OK := True;
-    end Set_Config;
+      procedure Set_D (M1, M2_5 : in Float) is
+      begin
+         D_M1 := M1;
+         D_M2_5 := M2_5;
+      end Set_D;
 
-    procedure Set_D (M1, M2_5 : in Float) is
-    begin
-      D_M1 := M1;
-      D_M2_5 := M2_5;
-    end Set_D;
+      procedure Set_D_M1 (Val : in Float) is
+      begin
+         D_M1 := Val;
+      end Set_D_M1;
 
-    procedure Set_D_M1 (Val : in Float) is
-    begin
-      D_M1 := Val;
-    end Set_D_M1;
+      procedure Set_D_M2_5 (Val : in Float) is
+      begin
+         D_M2_5 := Val;
+      end Set_D_M2_5;
 
-    procedure Set_D_M2_5 (Val : in Float) is
-    begin
-      D_M2_5 := Val;
-    end Set_D_M2_5;
+      procedure Set_Sim_Out (Val : in Sim_Output_T) is
+      begin
+         Sim_Out := Val;
+      end Set_Sim_Out;
 
-    procedure Set_Sim_Out (Val : in Sim_Output_T) is
-    begin
-      Sim_Out := Val;
-    end Set_Sim_Out;
+   end Simulation_I_T;
 
-  end Simulation_I_T;
-
-  task body Simulation_Task_T is
-    Angle      : Float := 0.0;
-    Load       : Float := 100.0;
-    Next_Time  : Time := Clock;
-    Conf       : Sim_Config_T;
-    Act, Prev  : Sim_Output_T;
-  begin
-    Sim.Wait_For_Config;
-    Conf := Sim.Get_Config;
-    loop
-      Act.I_L1   := Prev.I_L1 + (Sim.Get_D_M1 * abs (Prev.U_V1) - (1.0 - Sim.Get_D_M1) * Prev.U_C1) * Conf.T / Conf.L1;
-      Act.I_Load := Prev.U_C2 / Load;
-      Act.I_L2   := Prev.I_L2 + (Sim.Get_D_M2_5 * Prev.U_C1 - Prev.U_C2) * Conf.T / Conf.L2;
-      Act.U_C2   := (Prev.I_L2 - Prev.I_Load) * Conf.T / Conf.C2;
-      Act.U_C1   := (Prev.I_L1 - Sim.Get_D_M2_5 * Prev.I_L2) * Conf.T / Conf.C1;
-      if (Conf.f_V1 > 0.0001) then
-        Angle    := Float'Remainder ( (Angle + Conf.T * Conf.f_V1 * 2.0 * Ada.Numerics.Pi ), ( 2.0 * Ada.Numerics.Pi));
-        Act.U_V1 := Conf.Up_V1 *  Ada.Numerics.Elementary_Functions.Sin (Angle);
-      else
-        Act.U_V1 := Conf.Up_V1;
-      end if;
-      Sim.Set_Sim_Out (Act);
-      Ada.Text_IO.Put (Act.U_V1'Image & "  " & Act.U_C1'Image & "  " & Act.U_C2'Image);
-      Ada.Text_IO.Put_Line ("");
-      Prev       := Act;
-      Next_Time := Next_Time + Milliseconds (Integer (Float (Conf.T) * 1.0e6));
-      delay until Next_Time;
-    end loop;
-  end Simulation_Task_T;
-  Simuation_Task : Simulation_Task_T;
+   task body Simulation_Task_T is
+      Angle      : Float := 0.0;
+      Load       : Float := 100.0;
+      Next_Time  : Time := Clock;
+      Conf       : Sim_Config_T;
+      Act, Prev  : Sim_Output_T;
+   begin
+      Next_Time := Clock;
+      while (not Sim.Is_Ready)
+      loop
+         Next_Time := Next_Time + Milliseconds (200);
+         delay until Next_Time;
+      end loop;
+      Conf := Sim.Get_Config;
+      loop
+         Act.I_L1   := Prev.I_L1 +
+            (Sim.Get_D_M1 * abs (Prev.U_V1) -
+             (1.0 - Sim.Get_D_M1) * Prev.U_C1) * Conf.T / Conf.L1;
+         Act.I_Load := Prev.U_C2 / Load;
+         Act.I_L2   := Prev.I_L2 +
+            (Sim.Get_D_M2_5 * Prev.U_C1 - Prev.U_C2) * Conf.T / Conf.L2;
+         Act.U_C2   := (Prev.I_L2 - Prev.I_Load) * Conf.T / Conf.C2;
+         Act.U_C1   := (Prev.I_L1 - Sim.Get_D_M2_5 * Prev.I_L2)
+            * Conf.T / Conf.C1;
+         if (Conf.f_V1 > 0.0001) then
+            Angle    := Float'Remainder
+               ((Angle + Conf.T * Conf.f_V1 * 2.0 * Ada.Numerics.Pi),
+                (2.0 * Ada.Numerics.Pi));
+            Act.U_V1 := Conf.Up_V1 *
+               Ada.Numerics.Elementary_Functions.Sin (Angle);
+         else
+            Act.U_V1 := Conf.Up_V1;
+         end if;
+         Sim.Set_Sim_Out (Act);
+         Ada.Text_IO.Put (Act.U_V1'Image & "  " &
+                             Act.U_C1'Image & "  " & Act.U_C2'Image);
+         Ada.Text_IO.Put_Line ("");
+         Prev       := Act;
+         Next_Time := Next_Time + Milliseconds (Integer (1.0e6 * Conf.T));
+         delay until Next_Time;
+      end loop;
+   end Simulation_Task_T;
+   Simuation_Task : Simulation_Task_T;
 
 end PSU_Simulation;
--- src/psu_simulation.ads
+++ src/psu_simulation.ads
@@ -1,56 +1,55 @@
 pragma Profile (Ravenscar);
 package PSU_Simulation is
 
-  type Sim_Config_T is record 
-    L1     : Float := 1.0e-3;
-    C1     : Float := 1.0e-3;
-    L2     : Float := 1.0e-3;
-    C2     : Float := 1.0e-3;
-    f_V1   : Float := 1.0e-3;
-    Up_V1  : Float := 200.0;
-    T      : Float := 1.0;
-  end record;
-   
-  type Sim_Output_T is record
-    U_C2   : Float := 0.0;
-    I_Load : Float := 0.0;
-    I_L1   : Float := 0.0;
-    I_L2   : Float := 0.0;
-    U_C1   : Float := 0.0;
-    U_V1   : Float := 0.0;
-  end record;
-   
-  protected type Simulation_I_T is
-    entry     Wait_For_Config;
-    function  Is_Ready return Boolean;
-    function  Get_Config return Sim_Config_T;
-    function  Get_U_V1 return Float;
-    function  Get_I_L1 return Float;
-    function  Get_U_C1 return Float;    
-    function  Get_I_L2 return Float;
-    function  Get_U_C2 return Float;
-    function  Get_I_Load return Float;
-    function  Get_Sim_All return Sim_Output_T;
-    function  Get_D_M1 return Float;
-    function  Get_D_M2_5 return Float;
-    procedure Set_Config (Val : in Sim_Config_T);
-    procedure Set_D (M1, M2_5 : in Float);
-    procedure Set_D_M1 (Val : in Float);
-    procedure Set_D_M2_5 (Val : in Float);
-    procedure Set_Sim_Out (Val : in Sim_Output_T);
-  private
-    Sim_Out   : Sim_Output_T;
-    D_M2_5    : Float := 0.0;
-    D_M1      : Float := 0.0;
-    Conf      : Sim_Config_T;
-    Conf_OK   : Boolean := False;
-  end Simulation_I_T;
+   type Sim_Config_T is record
+      L1     : Float := 1.0e-3;
+      C1     : Float := 1.0e-3;
+      L2     : Float := 1.0e-3;
+      C2     : Float := 1.0e-3;
+      f_V1   : Float := 1.0e-3;
+      Up_V1  : Float := 200.0;
+      T      : Float := 1.0;
+   end record;
 
-Sim : Simulation_I_T;
-  
+   type Sim_Output_T is record
+      U_C2   : Float := 0.0;
+      I_Load : Float := 0.0;
+      I_L1   : Float := 0.0;
+      I_L2   : Float := 0.0;
+      U_C1   : Float := 0.0;
+      U_V1   : Float := 0.0;
+   end record;
+
+   protected type Simulation_I_T is
+      function  Is_Ready return Boolean;
+      function  Get_Config return Sim_Config_T;
+      function  Get_U_V1 return Float;
+      function  Get_I_L1 return Float;
+      function  Get_U_C1 return Float;
+      function  Get_I_L2 return Float;
+      function  Get_U_C2 return Float;
+      function  Get_I_Load return Float;
+      function  Get_Sim_All return Sim_Output_T;
+      function  Get_D_M1 return Float;
+      function  Get_D_M2_5 return Float;
+      procedure Set_Config (Val : in Sim_Config_T);
+      procedure Set_D (M1, M2_5 : in Float);
+      procedure Set_D_M1 (Val : in Float);
+      procedure Set_D_M2_5 (Val : in Float);
+      procedure Set_Sim_Out (Val : in Sim_Output_T);
+   private
+      Sim_Out   : Sim_Output_T;
+      D_M2_5    : Float := 0.0;
+      D_M1      : Float := 0.0;
+      Conf      : Sim_Config_T;
+      Conf_OK   : Boolean := False;
+   end Simulation_I_T;
+
+   Sim : Simulation_I_T;
+
 private
-  
-  task type Simulation_Task_T is
-  end Simulation_Task_T;
-   
+
+   task type Simulation_Task_T is
+   end Simulation_Task_T;
+
 end PSU_Simulation;
