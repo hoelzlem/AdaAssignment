742205e7763c323323e198d2215dc8bee57821b5
Simon Diehl <j.simon.diehl@googlemail.com>
2018-01-20 00:12:27 +0100

    Modified specification and definition to conform with the GNAT style checks

--- src/psu_monitoring.adb
+++ src/psu_monitoring.adb
@@ -1,4 +1,4 @@
-pragma Profile(Ravenscar);
+pragma Profile (Ravenscar);
 pragma SPARK_Mode;
 
 with Ada.Text_IO; use Ada.Text_IO;
@@ -8,212 +8,214 @@ with PSU_Simulation; use PSU_Simulation;
 
 package body PSU_Monitoring is
 
-    protected body Monitoring_Interface_T is
+   protected body Monitoring_Interface_T is
 
-        function is_all_config_set return Boolean is
-        begin
-            return monitor_pfc_voltage_config_set and monitor_pfc_current_config_set and monitor_output_voltage_config_set and monitor_output_current_config_set;
-        end is_all_config_set;
+      function is_all_config_set return Boolean is
+      begin
+         return monitor_pfc_voltage_config_set and monitor_pfc_current_config_set and monitor_output_voltage_config_set and monitor_output_current_config_set;
+      end is_all_config_set;
 
-        procedure set_monitor_pfc_voltage_config(new_monitor_config : in Monitor_Config_T) is
-        begin
-            monitor_pfc_voltage_config := new_monitor_config;
-            monitor_pfc_voltage_config_set := True;
-        end set_monitor_pfc_voltage_config;
+      procedure set_monitor_pfc_voltage_config (new_monitor_config : in Monitor_Config_T) is
+      begin
+         monitor_pfc_voltage_config := new_monitor_config;
+         monitor_pfc_voltage_config_set := True;
+      end set_monitor_pfc_voltage_config;
 
-        function get_monitor_pfc_voltage_config return Monitor_Config_T is
-        begin
-            return monitor_pfc_voltage_config;
-        end get_monitor_pfc_voltage_config;
+      function get_monitor_pfc_voltage_config return Monitor_Config_T is
+      begin
+         return monitor_pfc_voltage_config;
+      end get_monitor_pfc_voltage_config;
 
-        procedure set_monitor_pfc_current_config(new_monitor_config : in Monitor_Config_T) is
-        begin
-            monitor_pfc_current_config := new_monitor_config;
-            monitor_pfc_current_config_set := True;
-        end set_monitor_pfc_current_config;
+      procedure set_monitor_pfc_current_config (new_monitor_config : in Monitor_Config_T) is
+      begin
+         monitor_pfc_current_config := new_monitor_config;
+         monitor_pfc_current_config_set := True;
+      end set_monitor_pfc_current_config;
 
-        function get_monitor_pfc_current_config return Monitor_Config_T is
-        begin
-            return monitor_pfc_current_config;
-        end get_monitor_pfc_current_config;
+      function get_monitor_pfc_current_config return Monitor_Config_T is
+      begin
+         return monitor_pfc_current_config;
+      end get_monitor_pfc_current_config;
 
-        procedure set_monitor_output_voltage_config(new_monitor_config : in Monitor_Config_T) is
-        begin
-            monitor_output_voltage_config := new_monitor_config;
-            monitor_output_voltage_config_set := True;
-        end set_monitor_output_voltage_config;
+      procedure set_monitor_output_voltage_config (new_monitor_config : in Monitor_Config_T) is
+      begin
+         monitor_output_voltage_config := new_monitor_config;
+         monitor_output_voltage_config_set := True;
+      end set_monitor_output_voltage_config;
 
-        function get_monitor_output_voltage_config return Monitor_Config_T is
-        begin
-            return monitor_output_voltage_config;
-        end get_monitor_output_voltage_config;
+      function get_monitor_output_voltage_config return Monitor_Config_T is
+      begin
+         return monitor_output_voltage_config;
+      end get_monitor_output_voltage_config;
 
-        procedure set_monitor_output_current_config(new_monitor_config : in Monitor_Config_T) is
-        begin
-            monitor_output_current_config := new_monitor_config;
-            monitor_output_current_config_set := True;
-        end set_monitor_output_current_config;
+      procedure set_monitor_output_current_config (new_monitor_config : in Monitor_Config_T) is
+      begin
+         monitor_output_current_config := new_monitor_config;
+         monitor_output_current_config_set := True;
+      end set_monitor_output_current_config;
 
-        function get_monitor_output_current_config return Monitor_Config_T is
-        begin
-            return monitor_output_current_config;
-        end get_monitor_output_current_config;
+      function get_monitor_output_current_config return Monitor_Config_T is
+      begin
+         return monitor_output_current_config;
+      end get_monitor_output_current_config;
 
-    end Monitoring_Interface_T;
+   end Monitoring_Interface_T;
 
-    task body Monitoring_Task_T is
-        next_time : Time;
-    begin
-        -- Initialisation of next execution time
-        next_time := Clock;
-        -- Superloop
-        loop
-            Put_Line("Run task monitoring");
-            -- Load monitor configuration
-            monitor_pfc_voltage.config := monitoring_interface.get_monitor_pfc_voltage_config;
-            monitor_pfc_current.config := monitoring_interface.get_monitor_pfc_current_config;
-            monitor_output_voltage.config := monitoring_interface.get_monitor_output_voltage_config;
-            monitor_output_current.config := monitoring_interface.get_monitor_output_current_config;
+   task body Monitoring_Task_T is
+      next_time : Time;
+   begin
+      --  Initialisation of next execution time
+      next_time := Clock;
+      --  Superloop
+      loop
+         Put_Line ("Run task monitoring");
+         --  Load monitor configuration
+         monitor_pfc_voltage.config := monitoring_interface.get_monitor_pfc_voltage_config;
+         monitor_pfc_current.config := monitoring_interface.get_monitor_pfc_current_config;
+         monitor_output_voltage.config := monitoring_interface.get_monitor_output_voltage_config;
+         monitor_output_current.config := monitoring_interface.get_monitor_output_current_config;
 
-            -- Check if module has been configured correctly
-            -- Don't do anything otherwise
-            if monitoring_interface.is_all_config_set = True then
-                do_monitoring;
+         --  Check if module has been configured correctly
+         --  Don't do anything otherwise
+         if monitoring_interface.is_all_config_set = True then
+            do_monitoring;
+         end if;
+
+         next_time := next_time + TASK_PERIOD;
+         delay until next_time;
+      end loop;
+   end Monitoring_Task_T;
+
+   procedure do_monitoring is
+   begin
+      --  Monitor PFC intermediate voltage
+      monitor_signal (monitor_pfc_voltage, Sim.Get_U_C1);
+      --  Monitor PFC inductor current
+      monitor_signal (monitor_pfc_current, Sim.Get_I_L1);
+      --  Monitor output voltage
+      monitor_signal (monitor_output_voltage, Sim.Get_U_C2);
+      --  Monitor output inductor current
+      monitor_signal (monitor_output_current, Sim.Get_I_L2);
+   end do_monitoring;
+
+   procedure monitor_signal (monitor : in out Monitor_T; signal_value : in Float) is
+   begin
+      monitor.current_state := monitor.next_state;
+
+      case monitor.current_state is
+         when startup =>
+            --  Activate controllers
+            Ctrl.Set_Safety_State (True);
+
+            if is_within_limits (monitor, signal_value) = True then
+               monitor.next_state := settling;
+               monitor.timer := Milliseconds (0);
+            elsif monitor.timer >= monitor.config.startup_time then
+               monitor.next_state := shutdown;
+               monitor.timer := Milliseconds (0);
+            else
+               monitor.next_state := startup;
+               monitor.timer := monitor.timer + TASK_PERIOD;
             end if;
 
-            next_time := next_time + TASK_PERIOD;
-            delay until next_time;
-        end loop;
-    end Monitoring_Task_T;
+         when settling =>
+            if is_within_expanded_limits (monitor, signal_value) = False then
+               monitor.next_state := shutdown;
+               monitor.timer := Milliseconds (0);
+            elsif monitor.timer >= monitor.config.settling_time then
+               monitor.next_state := active;
+            else
+               monitor.next_state := settling;
+               monitor.timer := monitor.timer + TASK_PERIOD;
+            end if;
 
-    procedure do_monitoring is
-    begin    
-        -- Monitor PFC intermediate voltage
-        monitor_signal(monitor_pfc_voltage, Sim.Get_U_C1);
-        -- Monitor PFC inductor current
-        monitor_signal(monitor_pfc_current, Sim.Get_I_L1);
-        -- Monitor output voltage
-        monitor_signal(monitor_output_voltage, Sim.Get_U_C2);
-        -- Monitor output inductor current
-        monitor_signal(monitor_output_current, Sim.Get_I_L2);    
-    end do_monitoring;
+         when active =>
+            if is_within_limits (monitor, signal_value) = False then
+               monitor.next_state := alert;
+               monitor.timer := Milliseconds (0);
+            else
+               monitor.next_state := active;
+            end if;
 
-    procedure monitor_signal(monitor : in out Monitor_T; signal_value : in Float) is
-    begin
-        monitor.current_state := monitor.next_state;
+         when alert =>
+            if is_within_limits (monitor, signal_value) = True then
+               monitor.next_state := settling;
+               monitor.timer := Milliseconds (0);
+            elsif monitor.timer >= monitor.config.violation_time then
+               monitor.next_state := shutdown;
+               monitor.timer := Milliseconds (0);
+            else
+               monitor.next_state := alert;
+               monitor.timer := monitor.timer + TASK_PERIOD;
+            end if;
 
-        case monitor.current_state is
-            when startup =>
-                -- Activate controllers
-                Ctrl.Set_Safety_State(True);
+         when shutdown =>
+            --  Deactivate controllers
+            Ctrl.Set_Safety_State (False);
 
-                if is_within_limits(monitor, signal_value) = True then
-                    monitor.next_state := settling;
-                    monitor.timer := Milliseconds(0);
-                elsif monitor.timer >= monitor.config.startup_time then
-                    monitor.next_state := shutdown;
-                    monitor.timer := Milliseconds(0);
-                else
-                    monitor.next_state := startup;
-                    monitor.timer := monitor.timer + TASK_PERIOD;
-                end if;
+            if monitor.timer >= monitor.config.retry_time then
+               monitor.next_state := startup;
+               monitor.timer := Milliseconds (0);
+            else
+               monitor.next_state := shutdown;
+               monitor.timer := monitor.timer + TASK_PERIOD;
+            end if;
+      end case;
+   end monitor_signal;
 
-            when settling =>
-                if is_within_expanded_limits(monitor, signal_value) = False then
-                    monitor.next_state := shutdown;
-                    monitor.timer := Milliseconds(0);
-                elsif monitor.timer >= monitor.config.settling_time then
-                    monitor.next_state := active;
-                else
-                    monitor.next_state := settling;
-                    monitor.timer := monitor.timer + TASK_PERIOD;
-                end if;
+   function is_within_limits (monitor : in Monitor_T; signal_value : in Float) return Boolean is
+      within_limits : Boolean := False;
+   begin
+      case monitor.config.monitoring_mode is
+         when mean_based =>
+            if abs (monitor.config.mean - signal_value) <= monitor.config.maximum_deviation then
+               within_limits := True;
+            end if;
 
-            when active =>
-                if is_within_limits(monitor, signal_value) = False then
-                    monitor.next_state := alert;
-                    monitor.timer := Milliseconds(0);
-                else
-                    monitor.next_state := active;
-                end if;
+         when threshold_based =>
+            if signal_value >= monitor.config.lower_threshold or signal_value <= monitor.config.upper_threshold then
+               within_limits := True;
+            end if;
+      end case;
 
-            when alert =>
-                if is_within_limits(monitor, signal_value) = True then
-                    monitor.next_state := settling;
-                    monitor.timer := Milliseconds(0);
-                elsif monitor.timer >= monitor.config.violation_time then
-                    monitor.next_state := shutdown;
-                    monitor.timer := Milliseconds(0);
-                else
-                    monitor.next_state := alert;
-                    monitor.timer := monitor.timer + TASK_PERIOD;
-                end if;
+      return within_limits;
 
-            when shutdown =>
-                -- Deactivate controllers
-                Ctrl.Set_Safety_State(False);
+   end is_within_limits;
 
-                if monitor.timer >= monitor.config.retry_time then
-                    monitor.next_state := startup;
-                    monitor.timer := Milliseconds(0);
-                else
-                    monitor.next_state := shutdown;
-                    monitor.timer := monitor.timer + TASK_PERIOD;
-                end if;
-        end case;
-    end monitor_signal;
+   function is_within_expanded_limits (monitor : in Monitor_T; signal_value : in Float) return Boolean is
+      within_expanded_limits : Boolean := False;
 
-    function is_within_limits(monitor : in Monitor_T; signal_value : in Float) return Boolean is
-        within_limits : Boolean := False;
-    begin
-        case monitor.config.monitoring_mode is
-            when mean_based =>
-                if abs(monitor.config.mean - signal_value) <= monitor.config.maximum_deviation then
-                    within_limits := True;
-                end if;
+      expanded_lower_threshold : Float_Signed1000 := 0.0;
+      expanded_upper_threshold : Float_Signed1000 := 0.0;
+   begin
+      case monitor.config.monitoring_mode is
+         when mean_based =>
+            if abs (monitor.config.mean - signal_value) <= (monitor.config.maximum_deviation * monitor.config.settling_tolerance_expansion) then
+               within_expanded_limits := True;
+            end if;
 
-            when threshold_based =>
-                if signal_value >= monitor.config.lower_threshold or signal_value <= monitor.config.upper_threshold then
-                    within_limits := True;
-                end if;
-        end case;
+         when threshold_based =>
+            --  Calculate expanded thresholds
+            if monitor.config.lower_threshold >= 0.0 then
+               expanded_lower_threshold := monitor.config.lower_threshold / monitor.config.settling_tolerance_expansion;
+            else
+               expanded_lower_threshold := monitor.config.lower_threshold * monitor.config.settling_tolerance_expansion;
+            end if;
 
-        return within_limits;
-    end is_within_limits;
+            if monitor.config.upper_threshold >= 0.0 then
+               expanded_upper_threshold := monitor.config.upper_threshold * monitor.config.settling_tolerance_expansion;
+            else
+               expanded_upper_threshold := monitor.config.upper_threshold / monitor.config.settling_tolerance_expansion;
+            end if;
 
-    function is_within_expanded_limits(monitor : in Monitor_T; signal_value : in Float) return Boolean is
-        within_expanded_limits : Boolean := False;
+            --  Check limits with expanded thresholds
+            if signal_value >= expanded_lower_threshold or signal_value <= expanded_upper_threshold then
+               within_expanded_limits := True;
+            end if;
+      end case;
 
-        expanded_lower_threshold : Float_Signed1000 := 0.0;
-        expanded_upper_threshold : Float_Signed1000 := 0.0;
-    begin
-        case monitor.config.monitoring_mode is
-            when mean_based =>
-                if abs(monitor.config.mean - signal_value) <= (monitor.config.maximum_deviation * monitor.config.settling_tolerance_expansion) then
-                    within_expanded_limits := True;
-                end if;
+      return within_expanded_limits;
 
-            when threshold_based =>
-                -- Calculate expanded thresholds
-                if monitor.config.lower_threshold >= 0.0 then
-                    expanded_lower_threshold := monitor.config.lower_threshold / monitor.config.settling_tolerance_expansion;
-                else
-                    expanded_lower_threshold := monitor.config.lower_threshold * monitor.config.settling_tolerance_expansion;                        
-                end if;
-
-                if monitor.config.upper_threshold >= 0.0 then
-                    expanded_upper_threshold := monitor.config.upper_threshold * monitor.config.settling_tolerance_expansion;
-                else
-                    expanded_upper_threshold := monitor.config.upper_threshold / monitor.config.settling_tolerance_expansion;                        
-                end if;
-
-                -- Check limits with expanded thresholds
-                if signal_value >= expanded_lower_threshold or signal_value <= expanded_upper_threshold then
-                    within_expanded_limits := True;
-                end if;
-        end case;
-
-        return within_expanded_limits;
-    end is_within_expanded_limits;
+   end is_within_expanded_limits;
 
 end PSU_Monitoring;
--- src/psu_monitoring.ads
+++ src/psu_monitoring.ads
@@ -1,86 +1,86 @@
-pragma Profile(Ravenscar);
+pragma Profile (Ravenscar);
 pragma SPARK_Mode;
 
 with Ada.Real_Time; use Ada.Real_Time;
 
 package PSU_Monitoring is
 
-    type Monitoring_Mode_T is (mean_based, threshold_based);
-    type Monitor_State_T is (startup, settling, active, alert, shutdown);
-    
-    subtype Float_Natural1000 is Float range 0.0..1_000.0;
-    subtype Float_Positive10 is Float range 1.0..10.0;
-    subtype Float_Signed1000 is Float range -1_000.0..1_000.0;
+   type Monitoring_Mode_T is (mean_based, threshold_based);
+   type Monitor_State_T is (startup, settling, active, alert, shutdown);
 
-    type Monitor_Config_T is record
-        monitoring_mode : Monitoring_Mode_T := mean_based;
+   subtype Float_Natural1000 is Float range 0.0 .. 1_000.0;
+   subtype Float_Positive10 is Float range 1.0 .. 10.0;
+   subtype Float_Signed1000 is Float range -1_000.0 .. 1_000.0;
 
-        mean : Float_Signed1000 := 0.0;
-        maximum_deviation : Float_Natural1000 := 0.0;
-        lower_threshold : Float_Signed1000 := 0.0;
-        upper_threshold : Float_Signed1000 := 0.0;
+   type Monitor_Config_T is record
+      monitoring_mode : Monitoring_Mode_T := mean_based;
 
-        settling_tolerance_expansion : Float_Positive10 := 1.2;
+      mean : Float_Signed1000 := 0.0;
+      maximum_deviation : Float_Natural1000 := 0.0;
+      lower_threshold : Float_Signed1000 := 0.0;
+      upper_threshold : Float_Signed1000 := 0.0;
 
-        startup_time : Time_Span := Milliseconds(5);
-        settling_time : Time_Span := Milliseconds(2);
-        violation_time : Time_Span := Milliseconds(5);
-        retry_time : Time_Span := Milliseconds(100);
-    end record;
+      settling_tolerance_expansion : Float_Positive10 := 1.2;
 
-    type Monitor_T is record
-        config : Monitor_Config_T;
-        timer : Time_Span := Milliseconds(0);
-        current_state : Monitor_State_T := startup;
-        next_state : Monitor_State_T := startup;
-    end record;
+      startup_time : Time_Span := Milliseconds (5);
+      settling_time : Time_Span := Milliseconds (2);
+      violation_time : Time_Span := Milliseconds (5);
+      retry_time : Time_Span := Milliseconds (100);
+   end record;
 
-    protected type Monitoring_Interface_T is
-        function is_all_config_set return Boolean;
+   type Monitor_T is record
+      config : Monitor_Config_T;
+      timer : Time_Span := Milliseconds (0);
+      current_state : Monitor_State_T := startup;
+      next_state : Monitor_State_T := startup;
+   end record;
 
-        procedure set_monitor_pfc_voltage_config(new_monitor_config : in Monitor_Config_T);
-        function get_monitor_pfc_voltage_config return Monitor_Config_T;
-        
-        procedure set_monitor_pfc_current_config(new_monitor_config : in Monitor_Config_T);
-        function get_monitor_pfc_current_config return Monitor_Config_T;
-        
-        procedure set_monitor_output_voltage_config(new_monitor_config : in Monitor_Config_T);
-        function get_monitor_output_voltage_config return Monitor_Config_T;
-        
-        procedure set_monitor_output_current_config(new_monitor_config : in Monitor_Config_T);
-        function get_monitor_output_current_config return Monitor_Config_T;
-    private
-        -- Configuration for PFC intermediate voltage
-        monitor_pfc_voltage_config : Monitor_Config_T;
-        monitor_pfc_voltage_config_set : Boolean := False;
-        -- Configuration for PFC inductor current
-        monitor_pfc_current_config : Monitor_Config_T;
-        monitor_pfc_current_config_set : Boolean := False;
-        -- Configuration for output voltage
-        monitor_output_voltage_config : Monitor_Config_T;
-        monitor_output_voltage_config_set : Boolean := False;
-        -- Configuration for output inductor current
-        monitor_output_current_config : Monitor_Config_T;
-        monitor_output_current_config_set : Boolean := False;
-    end Monitoring_Interface_T;
+   protected type Monitoring_Interface_T is
+      function is_all_config_set return Boolean;
 
-    monitoring_interface : Monitoring_Interface_T;
-   
+      procedure set_monitor_pfc_voltage_config (new_monitor_config : in Monitor_Config_T);
+      function get_monitor_pfc_voltage_config return Monitor_Config_T;
+
+      procedure set_monitor_pfc_current_config (new_monitor_config : in Monitor_Config_T);
+      function get_monitor_pfc_current_config return Monitor_Config_T;
+
+      procedure set_monitor_output_voltage_config (new_monitor_config : in Monitor_Config_T);
+      function get_monitor_output_voltage_config return Monitor_Config_T;
+
+      procedure set_monitor_output_current_config (new_monitor_config : in Monitor_Config_T);
+      function get_monitor_output_current_config return Monitor_Config_T;
+   private
+      --  Configuration for PFC intermediate voltage
+      monitor_pfc_voltage_config : Monitor_Config_T;
+      monitor_pfc_voltage_config_set : Boolean := False;
+      --  Configuration for PFC inductor current
+      monitor_pfc_current_config : Monitor_Config_T;
+      monitor_pfc_current_config_set : Boolean := False;
+      --  Configuration for output voltage
+      monitor_output_voltage_config : Monitor_Config_T;
+      monitor_output_voltage_config_set : Boolean := False;
+      --  Configuration for output inductor current
+      monitor_output_current_config : Monitor_Config_T;
+      monitor_output_current_config_set : Boolean := False;
+   end Monitoring_Interface_T;
+
+   monitoring_interface : Monitoring_Interface_T;
+
 private
-    task type Monitoring_Task_T;
+   task type Monitoring_Task_T;
 
-    monitoring_task : Monitoring_Task_T;
+   monitoring_task : Monitoring_Task_T;
 
-    TASK_PERIOD : constant Time_Span := Milliseconds(100);
+   TASK_PERIOD : constant Time_Span := Milliseconds (100);
 
-    monitor_pfc_voltage : Monitor_T;
-    monitor_pfc_current : Monitor_T;
-    monitor_output_voltage : Monitor_T;
-    monitor_output_current : Monitor_T;
+   monitor_pfc_voltage : Monitor_T;
+   monitor_pfc_current : Monitor_T;
+   monitor_output_voltage : Monitor_T;
+   monitor_output_current : Monitor_T;
 
-    procedure do_monitoring;
-    procedure monitor_signal(monitor : in out Monitor_T; signal_value : in Float);
-    function is_within_limits(monitor : in Monitor_T; signal_value : in Float) return Boolean;
-    function is_within_expanded_limits(monitor : in Monitor_T; signal_value : in Float) return Boolean;
+   procedure do_monitoring;
+   procedure monitor_signal (monitor : in out Monitor_T; signal_value : in Float);
+   function is_within_limits (monitor : in Monitor_T; signal_value : in Float) return Boolean;
+   function is_within_expanded_limits (monitor : in Monitor_T; signal_value : in Float) return Boolean;
 
 end PSU_Monitoring;
