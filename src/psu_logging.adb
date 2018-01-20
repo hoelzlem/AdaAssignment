d02b5b4c2b66163c328ccad713352c414f56e8be
Simon Diehl <j.simon.diehl@googlemail.com>
2018-01-20 19:18:31 +0100

    Added generics (Check ;-) ) to the logging package but I get an error and don't know how to fix it yet. A stackoverflow question is live.

--- src/psu_logging.adb
+++ src/psu_logging.adb
@@ -4,6 +4,16 @@ with PSU_Simulation; use PSU_Simulation;
 
 package body PSU_Logging is
 
+   procedure csv_put (File : in File_Type; Item : in Item_Type_t) is
+   begin
+      Put (File, Item_Type_t'Image (Item));
+      Put (File, ", ");
+   end csv_put;
+
+   procedure csv_put_float is new csv_put (Item_Type_t => Float);
+   procedure csv_put_duration is new csv_put (Item_Type_t => Duration);
+   procedure csv_put_signal_name is new csv_put (Item_Type_t => logged_signal_names_t);
+
    task body logging_task is
       START_TIME : constant Time := Clock;
       next_time : Time := Clock;
@@ -11,14 +21,14 @@ package body PSU_Logging is
       output_file : File_Type;
    begin
       Create (File => output_file, Mode => Out_File, Name => "PSU.log");
+      write_header (output_file);
 
       loop
          Put_Line ("Running logging task");
          --  @TODO write a csv header that names the columns
          --  write all values from the simulation interface into the log file
          --  Write timestamp
-         csv_put (output_file, Duration'Image (To_Duration (next_time - START_TIME)));
-         csv_put_line (output_file, "end");
+         write_current_data (output_file, To_Duration (next_time - START_TIME));
 
          next_time := next_time + TASK_PERIOD;
          delay until next_time;
@@ -28,15 +38,23 @@ package body PSU_Logging is
          Put_Line (Standard_Error, "An exception occured");
    end logging_task;
 
-   procedure csv_put (File : in File_Type; Item : in String) is
+   procedure write_header (File : in File_Type) is
    begin
-      Put (File, Item);
-      Put (File, ", ");
-   end csv_put;
+      for signal_name in logged_signal_names_t range logged_signal_names_t'First .. logged_signal_names_t'Pred (logged_signal_names_t'Last) loop
+         csv_put_signal_name (File, signal_name);
+      end loop;
+      Put_Line (File, logged_signal_names_t'Image (logged_signal_names_t'Last));
+   end write_header;
 
-   procedure csv_put_line (File : in File_Type; Item : in String) is
+   procedure write_current_data (File : in File_Type; timestamp : in Duration) is
    begin
-      Put_Line (File, Item);
-   end csv_put_line;
+      csv_put_duration (File, timestamp);
+      csv_put_float (File, Sim.Get_U_V1);
+      csv_put_float (File, Sim.Get_I_L1);
+      csv_put_float (File, Sim.Get_U_C1);
+      csv_put_float (File, Sim.Get_I_L2);
+      csv_put_float (File, Sim.Get_U_C2);
+      Put_Line (File, Float'Image (Sim.Get_I_Load));
+   end write_current_data;
 
 end PSU_Logging;
--- src/psu_logging.ads
+++ src/psu_logging.ads
@@ -6,6 +6,16 @@ with System;
 
 package PSU_Logging is
 
+   type logged_signal_names_t is (
+      t,
+      U_V1,
+      I_L1,
+      U_C1,
+      I_L2,
+      U_C2,
+      I_Load
+   );
+
 private
    TASK_PERIOD : constant Time_Span := Milliseconds (100);
 
@@ -13,7 +23,11 @@ private
       pragma Priority (System.Priority'Last);
    end logging_task;
 
-   procedure csv_put (File : in File_Type; Item : in String);
-   procedure csv_put_line (File : in File_Type; Item : in String);
+   generic
+      type Item_Type_t is private;
+      procedure csv_put (File : in File_Type; Item : in Item_Type_t);
+
+   procedure write_header (File : in File_Type);
+   procedure write_current_data (File : in File_Type; timestamp : in Duration);
 
 end PSU_Logging;
