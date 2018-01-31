a51b44ece50f5c0db5ca6918aa68cd7c54d0476a
Simon Diehl <j.simon.diehl@googlemail.com>
2018-01-31 18:57:50 +0100

    * Added more signals to be logged as Alex suggested * Used access type for passing the output file to the logger from main => requirement fulfilled
    @TODO add logger configuration to main

--- src/psu_logging.adb
+++ src/psu_logging.adb
@@ -4,38 +4,58 @@ with PSU_Simulation; use PSU_Simulation;
 
 package body PSU_Logging is
 
-   procedure csv_put (File : in File_Type; Item : in Item_Type_t) is
+   protected body Logger_Interface_T is
+
+      function is_all_config_set return Boolean is
+      begin
+         return logfile_is_set;
+      end is_all_config_set;
+
+      procedure set_logfile (new_logfile : in File_CHandle) is
+      begin
+         logfile := new_logfile;
+         logfile_is_set := True;
+      end set_logfile;
+
+      function get_logfile return File_CHandle is
+      begin
+         return logfile;
+      end get_logfile;
+
+   end Logger_Interface_T;
+
+   procedure csv_put (File : in File_CHandle; Item : in Item_Type_t) is
    begin
-      Put (File, Image (Item));
-      Put (File, ", ");
+      Put (File.all, Image (Item));
+      Put (File.all, ", ");
    end csv_put;
 
    procedure csv_put_float is new csv_put (Item_Type_t => Float, Image => Float'Image);
    procedure csv_put_duration is new csv_put (Item_Type_t => Duration, Image => Duration'Image);
    procedure csv_put_signal_name is new csv_put (Item_Type_t => logged_signal_names_t, Image => logged_signal_names_t'Image);
 
-   procedure csv_end_line (File : in File_Type; Item : in Item_Type_t) is
+   procedure csv_end_line (File : in File_CHandle; Item : in Item_Type_t) is
    begin
-      Put_Line (File, Image (Item));
+      Put_Line (File.all, Image (Item));
    end csv_end_line;
 
    procedure csv_end_line_float is new csv_end_line (Item_Type_t => Float, Image => Float'Image);
    procedure csv_end_line_signal_name is new csv_end_line (Item_Type_t => logged_signal_names_t, Image => logged_signal_names_t'Image);
 
    task body logging_task is
-      START_TIME : constant Time := Clock;
       next_time : Time := Clock;
-
-      output_file : File_Type;
    begin
-      Create (File => output_file, Mode => Out_File, Name => "PSU.log");
+      while not logger_interface.is_all_config_set loop
+         next_time := next_time + TASK_PERIOD;
+         delay until next_time;
+      end loop;
 
-      write_header (output_file);
+      write_header (logger_interface.get_logfile);
 
       loop
-         Put_Line ("Running logging task");
+         Put_Line ("Running logging task: writing current data");
 
-         write_current_data (output_file, To_Duration (next_time - START_TIME));
+         write_current_data (logger_interface.get_logfile, To_Duration (next_time - Sim.Get_Start_Time));
 
          next_time := next_time + TASK_PERIOD;
          delay until next_time;
@@ -45,7 +65,7 @@ package body PSU_Logging is
          Put_Line (Standard_Error, "An exception occured");
    end logging_task;
 
-   procedure write_header (File : in File_Type) is
+   procedure write_header (File : in File_CHandle) is
    begin
       for signal_name in logged_signal_names_t range logged_signal_names_t'First .. logged_signal_names_t'Pred (logged_signal_names_t'Last) loop
          csv_put_signal_name (File, signal_name);
@@ -54,7 +74,7 @@ package body PSU_Logging is
       csv_end_line_signal_name (File, logged_signal_names_t'Last);
    end write_header;
 
-   procedure write_current_data (File : in File_Type; timestamp : in Duration) is
+   procedure write_current_data (File : in File_CHandle; timestamp : in Duration) is
    begin
       csv_put_duration (File, timestamp);
       csv_put_float (File, Sim.Get_U_V1);
@@ -62,7 +82,8 @@ package body PSU_Logging is
       csv_put_float (File, Sim.Get_U_C1);
       csv_put_float (File, Sim.Get_I_L2);
       csv_put_float (File, Sim.Get_U_C2);
-      csv_end_line_float (File, Sim.Get_I_Load);
+      csv_put_float (File, Sim.Get_I_Load);
+      csv_end_line_float (File, Sim.Get_Load);
    end write_current_data;
 
 end PSU_Logging;
